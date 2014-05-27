bba44573a8853c768839bd7a6f0dbc2e4a75473a
Anderson Marinho <T3GQ@ES00007125.petrobras.biz>
2014-05-27 14:22:49 -0300

    makeCycles algorithm with problem

--- src/br/ufrj/cos/expline/analysis/GraphStructure.java
+++ src/br/ufrj/cos/expline/analysis/GraphStructure.java
@@ -7,6 +7,9 @@ package br.ufrj.cos.expline.analysis;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import br.ufrj.cos.expline.model.Activity;
+import br.ufrj.cos.expline.model.Port;
+
 import com.mxgraph.analysis.mxAnalysisGraph;
 import com.mxgraph.analysis.mxGraphStructure;
 import com.mxgraph.model.mxCell;
@@ -25,7 +28,7 @@ public class GraphStructure extends mxGraphStructure
 		mxIGraphModel model = graph.getModel();
 		
 		//Workaround to add the request edge in the clone graph to verify that makes cycle
-		//TODO: Criar uma conexão a porta de saída e não com a entrada. (Vai funcionar só com atividades com uma porta de entrada e saída.)
+		//TODO: Criar uma conexão a porta de saída e não com a entrada. (Vai funcionar só com atividades com uma porta de entrada e saída.) (Não vai funcionar... Tem que fazer pra todas atividades)
 		Object parent = graph.getDefaultParent();
 		Object edge = graph.createEdge(parent, null, "", source, target, "");
 
@@ -47,8 +50,41 @@ public class GraphStructure extends mxGraphStructure
 			}
 		}
 		
+		
 		ArrayList<Object> cellList = new ArrayList<Object>(Arrays.asList(cells));
 		cellList.add(edge);
+		
+		for (int i = 0; i < cells.length; i++) {
+			mxCell temp = (mxCell)cells[i];
+			
+			if(temp instanceof Activity){
+				Activity actv = (Activity) temp;
+				
+				for (int j = 0; j < actv.getChildCount(); j++) {
+					Port port1 = (Port) actv.getChildAt(j);
+					
+					if(port1.getType() == Port.INPUT_TYPE){
+						for (int k = 0; k < actv.getChildCount(); k++) {
+							Port port2 = (Port) actv.getChildAt(k);
+							if(port2.getType() == Port.OUTPUT_TYPE){
+								Object edgeTemp = graph.createEdge(parent, null, "", port1, port2, "");
+								((mxCell)edgeTemp).setSource(port1);
+								((mxCell)edgeTemp).setTarget(port2);
+								cellList.add(edgeTemp);
+								break;
+							}
+						}
+					}
+				}
+				
+//				for (Port inputPort : actv.getInputPorts()) {
+//					Object edgeTemp = graph.createEdge(parent, null, "", inputPort, actv.getOutputPort(), "");
+//					
+//					cellList.add(edgeTemp);
+//				} 
+			}
+			
+		}
 		
 		//End of workaround
 		
