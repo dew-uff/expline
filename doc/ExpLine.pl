a8f4c377786f926a82b7e20debfd1fbd59d6688e
Anderson Marinho <t3gq@ES00007125.petrobras.biz>
2014-12-19 16:22:04 -0200

    versão funcional do código prolog mas ainda não funciona a parte que valida se uma solução é nó ou folha

--- doc/ExpLine.pl
+++ doc/ExpLine.pl
@@ -1,182 +1,66 @@
-/*
-Initialize derivation (Select mandatory activities to the abstract workflow) (Deveria tentar achar uma solução para as regras de composição e selecionar as variabilidades e opcionalidades)
-*/
-initializeDerivation(_) :-
-	findall(ActivityId,mandatory(ActivityId),ActivityIdList),
-	selectMandatoryActivities(ActivityIdList).
+selectElement([[E, true], Path]) :- write(E),
+    not isSelected(E,Path),
+    not isDesselected(E,Path),
+    write(' notSelected '),
+    write(Path),
+    assertz(currentSelection(E,Path, true)),
+%    write('Before flag '),
+%    flagAsIncomplete(Path),
+    write('Selected '),
+    rule([[E, true]|Path]), write('EndSelect ').
 
-/*
-Validate Derived Workflow
-*/
-isValidDerivedWorkflow(_) :-
-	findall(ActivityIdd,mandatory(ActivityIdd),MandatoryList),
-	findall(ActivityId,abstractWorkflow(ActivityId),AbstActvList),
-	containsAllMandatories(MandatoryList,AbstActvList),
-	findall(VPId,variationPoint(VPId),VPList),
-	checkVariationPoints(VPList,AbstActvList). 
-%	findall(RuleId,validationRule(RuleId),'[]').
-		
-validateRules('[]').
+selectElement([[E, false], Path]) :- write(E),
+    not isDesselected(E,Path),
+    not isSelected(E,Path),
+    write(' notSelected '),
+    write(Path),
+    assertz(currentDesselection(E,Path, true)),
+%    write('Before flag '),
+%    flagAsIncomplete(Path),
+    write('Selected '),
+    rule([[E, true]|Path]), write('EndSelect ').    
 
-validateRules([Rule|RuleList]) :-
-	validationRule(Rule),
-	validateRules(RuleList).    
+flagAsIncomplete([[E, true]| Path]):- write('flag1.1 '), write(E), retract(currentSelection(E, Path, true)), write('flag1.2 '), assertz(currentSelection(E, Path, false)), !.
+flagAsIncomplete([[E, true]| Path]):- write('flag1a.1 '), currentSelection(E, Path, false), write('flag1a.2 '), !.
+flagAsIncomplete([[E, false]| Path]):- write('flag2.1 '), retract(currentDesselection(E, Path, true)), write('flag2.2 '), assertz(currentDesselection(E, Path, false)), !.
+flagAsIncomplete([[E, false]| Path]):- write('flag2a.1 '), currentDesselection(E, Path, false), write('flag2a.2 '), !.
+flagAsIncomplete([]):- write('flag0').
+   
+isSelected(E,Path) :-
+    currentSelection(E,Path, _), 
+    !.
+   
+isSelected(E,[Head|Path]) :-
+    not currentSelection(E,[Head|Path], _),  
+    isSelected(E,Path).
+   
+isDesselected(E,Path) :-
+    currentDesselection(E,Path, _),
+    !.
+   
+isDesselected(E,[Head|Path]) :-
+    not currentDesselection(E,[Head|Path], _),
+    isDesselected(E,Path).
+   
+rule(Path) :- write('rule1 '),
+    (abstractWorkflow('B1') ; isSelected('B1',Path)) -> (add_to_queue([['E1', true], Path]) ; add_to_queue([['D1', true], Path])). 
+rule(Path) :- write('rule2 '),
+    (abstractWorkflow('D1') ; isSelected('D1',Path)) -> add_to_queue([['F1', true], Path]).
+rule(Path) :- write('rule3 '),
+    (abstractWorkflow('F1') ; isSelected('F1',Path)) -> add_to_queue([['B1', false], Path]).
+rule(Path).
 
-/*
-Verify if all mandatory activities are included in the abstract workflow.
-*/
-containsAllMandatories('[]',_).
+%queue([]).
+queue([[['B1', true], []]]).  
+%queue([[['B1'], []], [['D1'], ['B1']], [['F1'], ['D1', 'B1']]]).
 
-containsAllMandatories([M|MandatoryList],AbstActvList) :-
-	member(M,AbstActvList),
-	containsAllMandatories(MandatoryList,AbstActvList).
-
-/*
-Verify if one, and only one, variant is selected for every variation point.
-*/
-checkVariationPoints('[]',_).
-
-checkVariationPoints([VP|VPList],AbstActvList) :-
-	optional(VP),
-	not abstractWorkflow(VP),
-	checkVariationPoints(VPList,AbstActvList).    
-
-checkVariationPoints([VP|VPList],AbstActvList) :-
-	findall(VariantId,variant(VariantId,VP),VariantIdList),
-	isVariantSelected(VP,VariantIdList,AbstActvList),
-	not isThereAnyOtherVariantSelected(VP,VariantIdList,AbstActvList),
-	checkVariationPoints(VPList,AbstActvList).
-	
-
-isMoreVariantSelected(VariationPoint,[Var|VariantList],AbstActvList) :-
-	member(Var,AbstActvList),
-	not currentDeselection(Var).
-
-isMoreVariantSelected(VariationPoint,[Var|VariantList],AbstActvList) :-
-	member(Var,AbstActvList),
-	currentDeselection(Var),
-	isMoreVariantSelected(VariationPoint,VariantList,AbstActvList).
-
-isMoreVariantSelected(VariationPoint,[Var|VariantList],AbstActvList) :-
-	not member(Var,AbstActvList),
-	isMoreVariantSelected(VariationPoint,VariantList,AbstActvList).
-	
-
-isThereAnyOtherVariantSelected(VariationPoint,[Var|VariantList],AbstActvList) :-
-	member(Var,AbstActvList),
-	not currentDeselection(Var),
-	isMoreVariantSelected(VariationPoint,VariantList,AbstActvList).
-
-isThereAnyOtherVariantSelected(VariationPoint,[Var|VariantList],AbstActvList) :-
-	member(Var,AbstActvList),
-	currentDeselection(Var),
-	isThereAnyOtherVariantSelected(VariationPoint,VariantList,AbstActvList).
-
-isThereAnyOtherVariantSelected(VariationPoint,[Var|VariantList],AbstActvList) :-
-	not member(Var,AbstActvList),
-	isThereAnyOtherVariantSelected(VariationPoint,VariantList,AbstActvList).
-	
-/*
-Function to select all mandatory activities in the derived abstract workflow.
-*/
-selectMandatoryActivities('[]').
-
-selectMandatoryActivities([M|MandatoryList]) :-
-	assertz(abstractWorkflow(M)),
-	selectMandatoryActivities(MandatoryList).
-
-/*
-Regra de inserção pra mandatorio.
-*/
-selectElement(E) :-
-	not abstractWorkflow(E),
-	mandatory(E),
-	selectMandatoryActivities([E]). 
-
-selectElement(E) :-
-	abstractWorkflow(E),
-	mandatory(E).
-
-
-/*
-Regra de inserção pra ponto de variação opcional.
-*/
-selectElement(E) :-
-	not abstractWorkflow(E),
-	optional(E),
-	variationPoint(E),
-	assertz(abstractWorkflow(E)).
-
-selectElement(E) :-
-	abstractWorkflow(E),
-	optional(E),
-	variationPoint(E).
-
-unselectElement(E) :-
-	abstractWorkflow(E),
-	optional(E),
-	variationPoint(E),
-	retract(abstractWorkflow(E)). 
-
-/*
-Regra de inserção pra opcional nao ponto de variação.
-*/
-selectElement(E) :-
-	not abstractWorkflow(E),
-	optional(E),
-	assertz(abstractWorkflow(E)).
-
-selectElement(E) :-
-	abstractWorkflow(E),
-	optional(E).
-
-
-/*
-BEGIN: Regras de inserção e remoção de variantes
-*/
-%Não checa se tem mais de uma variante selecionada (isso nao acontece pq a funcao selectVariant nao existe) e da erro se o conjunto de variantes for vazio.
-isVariantSelected(VariationPoint,[Var|VariantList],AbstActvList) :-
-	member(Var,AbstActvList).
-
-isVariantSelected(VariationPoint,[Var|VariantList],AbstActvList) :-
-	not member(Var,AbstActvList),
-	isVariantSelected(VariationPoint,VariantList,AbstActvList).
-
-/*
-Function to select one variant of a variation point. Before select the functions checks if there is other variant already selected.
-*/
-selectVariant(Var) :-
-	findall(ActivityId,abstractWorkflow(ActivityId),AbstActvList),
-	variant(Var,VP),
-	findall(Var1,variant(Var1,VP),VariantList),
-	findall(ActivityId2,currentSelection(ActivityId2),CurrentSelectList),
-	append(AbstActvList,CurrentSelectList,CompleteList),
-	not isVariantSelected(VP,VariantList,CompleteList),
-	assertz(abstractWorkflow(Var)),
-	selectElement(VP).
-	
-selectElement(E) :-
-	not abstractWorkflow(E),
-	variant(E,_),
-	selectVariant(E).
-
-selectElement(E) :-
-	abstractWorkflow(E),
-	variant(E,_).
-
-/*
-acho que a desseleção ta errada, pois nao crio um caso quando o elemento jah esta desselecionado
-*/
-unselectElement(E) :-
-	abstractWorkflow(E),
-	variant(E,_583210292),
-	retract(abstractWorkflow(E)).
-
-
-and(A, B) :- A, B.
-imp(A, B) :- not(A); B.
-or(A, B) :- A ; B.
-nand(A,B) :- not(and(A,B)).
-xor(A, B) :- and(or(A, B), nand(A, B)).
-
-evaluate(E, true) :- E, !.
+add_to_queue(E) :- write('adicionando '), write(E), retract(queue(Queue)), add_to_queue2(E, Queue, NewQueue), assertz(queue(NewQueue)), !.
+add_to_queue2(E, [], [E]).
+add_to_queue2(E, [H|T], [H|Tnew]) :- add_to_queue2(E, T, Tnew). 
+   
+next_from_queue(E) :- write('proximo '), retract(queue(Queue)), next_from_queue2(E, Queue, NewQueue), write(E), assertz(queue(NewQueue)), !.
+       
+next_from_queue2(E, [E|T], T).            
+processImplications(_) :- write('BeforeQueue1 '), next_from_queue(Next), write('AfterQueue '), selectElement(Next).
+%processImplications(_) :- write('BeforeQueue1 '), next_from_queue([[E, true], Path]), write('AfterQueue '), flagAsIncomplete(Path), selectElement([[E, true], Path]).
+processImplications(_). 
